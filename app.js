c487c8cf5397bde0ed2fc97f7d247470b50d84f5
donghee <handh0413@naver.com>
2019-12-29 19:38:02 +0900

    lesson-3

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-// vue-instance 객체를 생성해야 한다.
-// Vue 객체는 Vue JS에서 제공하는 기본 객체다.
 new Vue({
-    el: '#vue-app', // Vue 인스턴스가 Control하는 HTML 요소(Root Element)
+    el: '#vue-app', 
     data: {
-      name: 'Shaun'  
-    } // data 프로퍼티는 Vue 인스턴스에서 사요할 데이터 정보를 관리한다.
+      name: 'Shaun',
+      job: 'Ninja'
+    },
+    methods: {
+        greet: function(time) {
+            // this 키워드를 이용하여 data 프로퍼티의 데이터에 접근 가능
+            return 'Good ' + time +  ' ' + this.name; 
+        }
+    } // Vue 인스턴스 관련 메소드를 작성할 수 있다.
 });
