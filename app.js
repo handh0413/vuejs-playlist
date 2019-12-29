db0b4225b89ce2b2e5b3c8148ae0a4acf794c793
donghee <handh0413@naver.com>
2019-12-29 19:51:33 +0900

    lesson-4

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ new Vue({
     el: '#vue-app', 
     data: {
       name: 'Shaun',
-      job: 'Ninja'
+      job: 'Ninja',
+      website: 'https://www.naver.com',
+      websiteTag: '<a href="https://www.daum.net">다음</a>'
     },
     methods: {
         greet: function(time) {
-            // this 키워드를 이용하여 data 프로퍼티의 데이터에 접근 가능
             return 'Good ' + time +  ' ' + this.name; 
         }
-    } // Vue 인스턴스 관련 메소드를 작성할 수 있다.
+    }
 });
